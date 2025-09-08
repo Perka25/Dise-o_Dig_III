bdff3a5acc5afa110576c392ef36b5160bf08f1d
Perka25 <frayre251629@gmail.com>
2025-09-08 12:34:37 -0600

    [AGREGA]Se agrego mas al formulario y una imagen a la carpeta de recursos

--- Formularios/js/formulario.js
+++ Formularios/js/formulario.js
@@ -1,8 +1,13 @@
 const formulario_producto_nuevo = document.querySelector("#formulario_producto_nuevo");
 
-class Producto {
-    constructor(id, nombre, descripcion, precio){
+let productos = [];//estoy declarando un arreglo vacio
 
+let src_imagen_producto = "../recursos/default-image.jpg";
+
+class Producto {
+    constructor(id, imagen, nombre, descripcion, precio){
+
+        this.Imagen = imagen;
         this.Id = id;
         this.Nombre = nombre;
         this.Descripcion = descripcion;
@@ -11,6 +16,7 @@ class Producto {
 
     ObtenerDatos(){
         console.log(this.Id);
+        console.log(this.Imagen);
         console.log(this.Nombre);
         console.log(this.Descripcion);
         console.log(this.Precio);
@@ -19,8 +25,31 @@ class Producto {
 
 function AgregarProducto(event){
 
-    let lectorFormulario = new FormData(formulario_producto_nuevo);
-    const datos = Object.fromEntries(lectorFormulario.entries());
+    let datosFormulario = new FormData(formulario_producto_nuevo);
+    const datos = Object.fromEntries(datosFormulario.entries());
+    console.log(datos)
+    if(datos.Nombre != "" && datos.Descripcion != " " && datos.Precio != null)
+    productos.push(new Producto(productos.length+1, datos.Nombre, datos.Descripcion, datos.Precio, datos.Imagen));
+
+    productos.forEach(producto =>{
+        producto.ObtenerDatos();
+    });
+
     const json = JSON.stringify(datos);    
     console.log(json);
+}
+
+function obtenerImagen(event){
+    const file = event.target.files[0];
+    if(file.type === "image/jpeg" || file.type === "image/png"){
+        console.log (file.name);
+
+        const lector = new FileReader();
+
+        lector.onload = (event) =>{
+            src_imagen_producto = event.target.result;
+            document.querySelector("#imagen-file").src = src_imagen_producto;
+        }
+        lector.readAsDataURL(file);
+    }
 }
